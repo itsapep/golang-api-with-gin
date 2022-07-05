7dfc58b55392899888d503b69c4b6b12fa83487c
Yurham Afif <yurhamafif.edu@gmail.com>
2022-07-05 14:01:18 +0700

    fix slice problem

--- manager/repository_manager.go
+++ manager/repository_manager.go
@@ -1,6 +1,9 @@
 package manager
 
-import "github.com/itsapep/golang-api-with-gin/repository"
+import (
+	"github.com/itsapep/golang-api-with-gin/model"
+	"github.com/itsapep/golang-api-with-gin/repository"
+)
 
 type RepositoryManager interface {
 	// collection of all repo
@@ -8,11 +11,12 @@ type RepositoryManager interface {
 }
 
 type repositoryManager struct {
+	productDb []model.Product
 }
 
 // ProductRepo implements RepositoryManager
-func (*repositoryManager) ProductRepo() repository.ProductRepository {
-	return repository.NewProductRepository()
+func (r *repositoryManager) ProductRepo() repository.ProductRepository {
+	return repository.NewProductRepository(&r.productDb)
 }
 
 func NewRepositoryManager() RepositoryManager {
--- repository/product_repository.go
+++ repository/product_repository.go
@@ -8,20 +8,21 @@ type ProductRepository interface {
 }
 
 type productRepository struct {
-	productDb []model.Product
+	productDb *[]model.Product
 }
 
 // Retrieve implements ProductRepository
 func (p *productRepository) Retrieve() ([]model.Product, error) {
-	return p.productDb, nil
+	return *p.productDb, nil
 }
 
 func (p *productRepository) Add(newProduct *model.Product) error {
-	p.productDb = append(p.productDb, *newProduct)
+	*p.productDb = append(*p.productDb, *newProduct)
 	return nil
 }
 
-func NewProductRepository() ProductRepository {
+func NewProductRepository(productDb *[]model.Product) ProductRepository {
 	repo := new(productRepository)
+	repo.productDb = productDb
 	return repo
 }
